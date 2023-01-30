8e3a4c93f4ce3490d2a2c2e196cc2361a488b526
MinWoo <mynooo7688@gmail.com>
2023-01-30 14:19:58 +0900

    Initial Commit

---
+++ MyBuilding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBuilding
+//
+//  Created by 강민우 on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
